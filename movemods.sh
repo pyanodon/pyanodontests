2dd14555f7647850773c2794fc69616e3b404bb5
Alex ten Brink <alextenbrink@gmail.com>
2025-02-10 22:50:04 +0100

    Another commit, another attempt at a fix

--- movemods.sh
+++ movemods.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
-cp './mod-list.json' './factorio/mods/mod-list.json'
+mkdir -p ./factorio/mods
+cp './mod-list.json' ./factorio/mods/
 jq -c '.[]' mods.json |
 while read -r i; do
     mod_repo=$(echo "$i" | jq -cr '.repository')
     mod_name=$(echo "$i" | jq -cr '.name')
 
     if [[ -d ./"$mod_repo" ]]; then
-        mkdir -p ./factorio/mods/"$mod_name"
+        mkdir ./factorio/mods/"$mod_name"
         mv ./"$mod_repo"/* ./factorio/mods/"$mod_name"
         rm -r ./"$mod_repo"
     fi
