b4c5fb7f5b85f0903dc7fa985ce5d70fbd792a38
Zachary Picco <zacharypicco@gmail.com>
2025-09-13 21:10:46 -0500

    pypp dev mode is no longer a real setting.

--- tests/crashtest/testmod/settings-updates.lua
+++
@@ -1,5 +0,0 @@
-if mods["pypostprocessing"] then
-    data.raw["bool-setting"]["pypp-dev-mode"].forced_value  = false
-    data.raw["bool-setting"]["pypp-create-cache"].forced_value  = false
-    data.raw["bool-setting"]["pypp-verbose-logging"].forced_value  = false
-end
